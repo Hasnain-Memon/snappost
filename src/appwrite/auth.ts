9121189f8b9520395897eee00bc4d0cec5b5c3e6
Hasnain Memon <160097523+Hasnain-Memon@users.noreply.github.com>
2024-06-16 14:48:37 -0700

    add auth & database service

---
+++ src/appwrite/auth.ts
@@ -0,0 +1,64 @@
+import {Client, Account, ID} from "appwrite";
+import conf from "../conf/conf";
+
+
+class AuthService {
+  client = new Client();
+  account;
+
+  constructor(){
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+
+  async createAccount(email: string, password: string, name: string) {
+    try {
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+      if (userAccount) {
+        // call login function here
+        this.login(email, password);
+        return userAccount;
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("Error while creating account:", error);
+      throw error;
+    }
+  }
+
+  async login(email: string, password: string) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log("Error while logging user:", error);
+      throw error;
+    }
+  }
+
+  async getCurrentUser(){
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Error while getting current user:", error);
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Error while logging out user:", error);
+      throw error;
+    }
+  }
+
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ src/appwrite/database.ts
@@ -0,0 +1,143 @@
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+import conf from "../conf/conf.ts";
+
+class DatabaseService {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost(title: string, content: string, featuredIamge: string, slug: string, status: string, userId: string) {
+        try {
+            const post = await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredIamge,
+                    status,
+                    userId
+                }
+            )
+            return post;
+        } catch (error) {
+            console.log("Error while creating post:", error);
+            throw error;
+        }
+    }
+
+    async updatePost(slug: string, title: string, content: string, featuredIamge: string, status: string) {
+        try {
+            const post = await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredIamge,
+                    status
+                }
+            )
+            return post;
+        } catch (error) {
+            console.log("Error while updating post:", error);
+            throw error;
+        }
+    }
+
+    async deletePost(slug: string) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Error while deleting post:", error);
+            throw error;
+        }
+    }
+
+    async getPost(slug: string) {
+        try {
+            const post = await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return post;
+        } catch (error) {
+            console.log("Error while geting post:", error);
+            throw error;
+        }
+    }
+
+    async getAllPost(queries = [Query.equal("status", "active")]) {
+        try {
+            const AllPosts = await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+            return AllPosts;
+        } catch (error) {
+            console.log("Error while getting All post:", error);
+            throw error;
+        }
+    }
+
+    // file services starts here
+
+    async uploadFile(file: File) {
+        try {
+            const uploadedFile = await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+            return uploadedFile;
+        } catch (error) {
+            console.log("Error while uploading file:", error);
+            throw error;
+        }
+    }
+
+    async deleteFile(fileId: string) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            console.log("Error while deleting file:", error);
+            throw error;
+        }
+    }
+
+    async getFilepreview(fileId: string) {
+        try {
+        return await this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+        } catch (error) {
+            console.log("Error while getting file preview file:", error);
+            throw error;
+        }
+    }
+
+}
+
+const databaseService = new DatabaseService();
+
+export default databaseService;
