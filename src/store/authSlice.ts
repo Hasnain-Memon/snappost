b8cb731c9624d87034f4ad61ed9564049f2914aa
Hasnain Memon <160097523+Hasnain-Memon@users.noreply.github.com>
2024-06-16 17:30:23 -0700

    configured store

---
+++ src/store/authSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+interface AuthState {
+    status: boolean,
+    userData: any
+}
+
+const initialState: AuthState = {
+    status: false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
+})
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
